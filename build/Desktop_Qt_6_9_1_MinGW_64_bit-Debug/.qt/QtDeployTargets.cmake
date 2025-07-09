ab2cbdf98cb5e73b11430fe2f87d440c760f2bce
Evgeniy Promashkov <evgenproma@mail.ru>
2025-07-09 22:52:42 +0300

    Remove build directory

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_audioFileAnalyzer_FILE D:/audioFileAnalyzer/audioFileAnalyzer/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/audioFileAnalyzer.exe)
-set(__QT_DEPLOY_TARGET_audioFileAnalyzer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_audioFileAnalyzer_RUNTIME_DLLS D:/Qt/6.9.1/mingw_64/bin/Qt6Multimedia.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Charts.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Network.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6OpenGLWidgets.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6OpenGL.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
